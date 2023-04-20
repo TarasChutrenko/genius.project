a987f65036525d75ae8469df694e28168c469a8a
Taras Chutrenko <sportfan270@gmail.com>
2023-04-20 16:19:39 +0300

    added homework6

---
+++ Lesson6-main/Lesson/homework6.js
@@ -0,0 +1,48 @@
+// Домашнє завдання
+// Зробити телефону книгу:
+// створити початковий клас Abonent,
+// де зберігатимуться ім*я і номер 
+// створити set який прийматиме телефон і номер
+// створити get який виводитиме данні про абонента
+// створити три різних юзери 
+// вивести данні 
+
+// class Abonent {
+//     constructor(options) {
+//         this.name = options.name;
+//         this.phone = options.phone;
+//     }
+
+//     name = "";
+//     phone = "";
+  
+// set name(newName) {
+//     const name = newName;
+//     this.name = name;
+//     this.phone = phone;
+// }
+
+// get name() {
+//     return ` Name : ${this.name} Phone : ${this.phone}`;
+// }
+// }
+
+// const Art = new Abonent({
+//     name: "Artem",
+//     phone: "+380952378968",
+// });
+
+// const Val = new Abonent({
+//     name: "Valya",
+//     phone: "+3809523734527",
+// });
+
+// const Mom = new Abonent({
+//     name: "Mother",
+//     phone: "+380992218960",
+// });
+
+// console.log(Art);
+// console.log(Val);
+// console.log(Mom);
+
